063c15ea8c4d5e888ed8df5339e38dd090acb965
Asher Vitek <asher.vitek@gmail.com>
2024-11-27 15:20:53 -0600

    fixing the issue where when aspmonkey is not present that it is throwing an error.

--- lua/config/usercmd.lua
+++ lua/config/usercmd.lua
@@ -6,5 +6,8 @@ end
 
 vim.api.nvim_create_user_command('FormatRuleXml', format_rule_xml, { desc = 'Format Rule Xml' })
 vim.api.nvim_create_user_command('FormatElmahError', '%s/&#xD;&#xA;/\r/g', { desc = 'Format Elmah Error' })
-vim.api.nvim_create_user_command('AspMonkey', require("aspmonkey").asp_monkey,
-    { desc = 'Monkey me some ASP into one big file.' })
+
+if pcall(require, "aspmonkey") then
+    vim.api.nvim_create_user_command('AspMonkey', require("aspmonkey").asp_monkey,
+        { desc = 'Monkey me some ASP into one big file.' })
+end
