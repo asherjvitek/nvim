8b713129501df90d2ff2830091d543d0c8526f03
Asher Vitek <asher.vitek@gmail.com>
2024-12-11 21:15:59 -0600

    I did the multigrep thing! Finally!

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -82,10 +82,18 @@ return {
         vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
             { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+
+        vim.keymap.set('n', '<leader>sg', require("config.multigrep").live_multigrep, { desc = '[S]earch by [G]rep' })
+
         vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
         vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics,
             { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+        vim.keymap.set('n', '<leader>sp', function()
+            require('telescope.builtin').find_files {
+                cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+            }
+        end, { desc = '[S]earch [P]lugins' })
     end
 }
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -12,5 +12,13 @@ return {
         config = function()
             vim.cmd.colorscheme 'vscode'
         end,
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        -- priority = 1000,
+        -- config = function()
+        --     vim.cmd.colorscheme 'catppuccin'
+        -- end,
     }
 }
