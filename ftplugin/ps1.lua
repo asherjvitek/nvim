80c232a91bdef0cb9c588cd465a8ee708fa7ed5e
Asher Vitek <asher.vitek@gmail.com>
2025-03-13 09:15:47 -0500

    Adding a little config to make the below window scroll when I would execute a command using the eval command

--- ftplugin/ps1.lua
+++ ftplugin/ps1.lua
@@ -1,2 +1,20 @@
-vim.keymap.set("n", "<leader>tp", function() require("powershell").toggle_term() end, { desc = "[T]oggle Powsershell Terminal" })
-vim.keymap.set({ "n", "v" }, "<leader>E", function() require("powershell").eval() end, { desc = "[E]val Powershell" })
+vim.keymap.set("n", "<leader>tp", function()
+        require("powershell").toggle_term()
+    end,
+    { desc = "[T]oggle Powsershell Terminal" }
+)
+
+vim.keymap.set({ "n", "v" }, "<leader>E", function()
+    require("powershell").eval()
+
+    --This sorta works. I think that we need some better hook that is on lsp done or something.
+    local util = require("powershell.util")
+    local buf = vim.api.nvim_get_current_buf()
+    local term_buf = util.term_buf(buf)
+    if term_buf ~= nil then
+        vim.api.nvim_buf_call(term_buf, function()
+            vim.cmd(":norm G")
+        end)
+    end
+end, { desc = "[E]val Powershell" }
+)
