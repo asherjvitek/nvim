a328c6e3cab4c753b6ec751775438c4adbbb0166
Asher Vitek <asher.vitek@gmail.com>
2025-07-29 15:25:47 -0500

    removing some themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,24 +1,9 @@
 return {
     {
-        'folke/tokyonight.nvim',
-        -- priority = 1000,
-        -- config = function()
-        --     vim.cmd.colorscheme 'tokyonight'
-        -- end,
-    },
-    {
         'Mofiqul/vscode.nvim',
         priority = 1000,
         config = function()
             vim.cmd.colorscheme 'vscode'
         end,
-    },
-    {
-        "catppuccin/nvim",
-        -- name = "catppuccin",
-        -- priority = 1000,
-        -- config = function()
-        --     vim.cmd.colorscheme 'catppuccin'
-        -- end,
     },
 }
